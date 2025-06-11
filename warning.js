e45f6df9898d5d7707572cb0a19618a15de75b02
andika7879 <buisniscoc@gmail.com>
2025-06-11 10:37:26 +0700

    Add files via upload

---
+++ warning.js
@@ -0,0 +1,29 @@
+Thanks To
+- Gitzzz            (Pencipta) 
+- Kyuu             (Base) 
+- Putra             (Friend) 
+- Alwasyanan      (Friend) 
+- Zuu/Shinzo       (Friend) 
+- Drak - Angel     (Support) 
+- Vyn              (support) 
+
+
+         Peringatan Pengguna script
+- harus menggunakan Node 20 di panel
+- No ubah ubah enc nya ntar eror
+- habis bug restart panel nya biar bisa bug lagi
+- free functionn di start-lib-functionnew
+         
+         INFO SCRIPT
+- Smith's' V1 masih pengembangan wajar kalau gak ada
+all bug yg sering di pake yaitu bug grup dan bug grup ada di
+v2 nya dan v2 nya akan update lagi nanti kalau script ini rame
+bakal up scriptnya v2 gatau free apa buy ygy insyaallah v2 gg
+
+   Kalau mau nanya - nanya silahkan Chat dan Join channel nya
+- whatsapp : wa.me/6282228095170
+- tele      : t.me/GitzzVyn
+- channel :"https://whatsapp.com/channel/0029Vay2AJM2phHThRkxrT1w"
+dan bantu ramein biar semangat
+
+TERIMAKASIH
